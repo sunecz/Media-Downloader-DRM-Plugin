ee22922b6df4d5aca6518c103e45198552493ba2
Sune <sune@suneweb.net>
2022-07-12 17:07:24 +0200

    Released version 0005.

--- src/sune/app/mediadownloader/drm/integration/DRMPlugin.java
+++ src/sune/app/mediadownloader/drm/integration/DRMPlugin.java
@@ -9,7 +9,7 @@ import sune.app.mediadownloader.drm.DRMInstance;
 
 @Plugin(name          = "drm",
 		title         = "plugin.drm.title",
-		version       = "0004",
+		version       = "0005",
 		author        = "Sune",
 		updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/plugin/drm/",
 		updatable     = true,
