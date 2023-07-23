85f8c2173d40ed24fe3d9ee5a5ba233b2e9042bf
Sune <sune@suneweb.net>
2023-07-23 18:48:19 +0200

    Increased version to 00.02.09-0005.

--- src/sune/app/mediadown/drm/DRMPlugin.java
+++ src/sune/app/mediadown/drm/DRMPlugin.java
@@ -7,7 +7,7 @@ import sune.app.mediadown.plugin.PluginBase;
 
 @Plugin(name          = "drm",
 		title         = "plugin.drm.title",
-		version       = "00.02.09-0004",
+		version       = "00.02.09-0005",
 		author        = "Sune",
 		updateBaseURL = "https://app.sune.tech/mediadown/dat/plugin/0002/plugin/drm/",
 		updatable     = true,
